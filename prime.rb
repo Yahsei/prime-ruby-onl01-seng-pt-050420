45d24e6fad8291a001e504dd1d57cc7061396575
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-13 18:23:32 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,13 @@
+def is_prime?(*nums)
+    nums.each do |num|
+    i = 2
+      while i < num
+        if num % i == 0
+          puts "#{num} is not a prime"
+        else
+          puts "#{num} is a prime"
+        end
+        i += 1
+      end
+   end
+end
